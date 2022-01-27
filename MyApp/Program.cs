c75d0e1ac9c2994996224fe48da6369ea214385b
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-01-27 15:22:11 +0100

    following the tutorial

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,4 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Humanizer;
+Console.WriteLine("Quantities:");
+HumanizeQuantities();
+
+Console.WriteLine("\nDate/Time Manipulation:");
+HumanizeDates();
+
+/*
 using System;
 
 public class Program
@@ -7,4 +15,18 @@ public class Program
   {
     Console.WriteLine("Hello world!");
   }
+}
+*/
+
+static void HumanizeQuantities() {
+    Console.WriteLine("case".ToQuantity(0));
+    Console.WriteLine("case".ToQuantity(1));
+    Console.WriteLine("case".ToQuantity(5));
+}
+
+static void HumanizeDates() {
+    Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+    Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(16).Humanize());
 }
